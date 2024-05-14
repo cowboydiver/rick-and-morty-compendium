a91e5ebe39f0cb33476918ebd4da6cf66943e31a
Jan <jandetlefsen@gmail.com>
2024-05-14 16:25:42 +0200

    feat(rick-and-morty): :sparkles: fetch list of characters from api

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,31 @@
+"use client";
+
 import { Accordion, AccordionButton, AccordionIcon, AccordionItem, AccordionPanel, Box, Card, Flex, Text, theme } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+
+
+const endpoint = "https://rickandmortyapi.com/api/character";
+
+async function getData() {
+  const res = await fetch(endpoint)
+ 
+  if (!res.ok) {
+    throw new Error('Failed to fetch data')
+  }
+ 
+  return res.json()
+}
 
 export default function Home() {
+
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    getData().then(result => {
+      setData(result.results)
+      console.log(result)})
+  }, [])
+
   return (
     <Box p="5" w="100vw" h="100vh" bg={theme.colors.gray[200]}>
       <Card>
