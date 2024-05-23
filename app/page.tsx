9f77762d5b88a55bd51644c2b60c7ee52d1413b0
Jan <jandetlefsen@gmail.com>
2024-05-23 20:49:30 +0200

    refactor(rick-and-morty): :recycle: search form now uses a form for onsubmit

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 "use client";
 
-import { Box, Button, Flex, Input, InputGroup, InputLeftElement, Text, useTheme } from "@chakra-ui/react";
-import { useEffect, useState } from "react";
+import { Box, Button, Flex, FormControl, FormErrorMessage, Input, Text, useTheme } from "@chakra-ui/react";
+import { useEffect, useState} from "react";
+import { useForm } from 'react-hook-form'
 import { Character, getCharacters } from "rickmortyapi"; //Use this to get types and easier access to the API
 import CharacterTable from "./components/CharacterTable";
 
@@ -9,7 +10,7 @@ export default function Home() {
 
   const theme = useTheme()
 
-  const [data, setData] = useState<Character[] | undefined>([])
+  const [data, setData] = useState<Character[]>([])
 
   const [search, setSearch] = useState<string>("")
 
@@ -19,18 +20,25 @@ export default function Home() {
 
   useEffect(() => {
     gotoPage(1)
-  },[])
+  },[]) 
 
-  function handleSearch(event: React.ChangeEvent<HTMLInputElement>) {
-    setSearch(event.target.value)
-    console.log(search)
+  const {
+    handleSubmit,
+    register,
+    formState: { errors, isSubmitting },
+  } = useForm()
+
+  function onSubmit(values: any) {
+    const name = values.name //You shouldn't use any, but I'm lazy right now
+    searchCharacters(name)
   }
 
   // Use a dedicated function to search for characters as to not call the api each time user types
-  function searchCharacters() {
+  function searchCharacters(queryName: string) {
     setCurrentPage(1)
-    getCharacters({ name: search, page: 1}).then(result => {
-      setData(result.data.results?.map((item: Character) => item))
+    getCharacters({ name: queryName, page: 1}).then(result => {
+      setSearch(queryName) //remember the search query for pagination 
+      setData(result.data.results?.map((item: Character) => item) ?? []) //Maybe use the Info interface to get more data
       setTotalPages(result.data.info?.pages ?? 1)
     }).catch(err => {
       console.log(err)
@@ -40,7 +48,7 @@ export default function Home() {
   function gotoPage(newPage: number) {
     setCurrentPage(newPage)
     getCharacters({ name: search, page: newPage }).then(result => {
-      setData(result.data.results?.map((item: Character) => item))
+      setData(result.data.results?.map((item: Character) => item) ?? [])
       setTotalPages(result.data.info?.pages ?? 1)
     }).catch(err => {
       console.log(err)
@@ -51,10 +59,17 @@ export default function Home() {
   return (
     <Box p="5" w="100vw" h="100vh" bg={theme.colors.gray[200]} overflow="scroll">
       <Text fontSize="3xl" mb="5">Rick and Morty Characters</Text>
-      <Flex direction="row">
-        <Input variant="solid" placeholder="Search character name" mb="5" maxW="400px" onChange={handleSearch}/>
-        <Button colorScheme="green" ml="5" onClick={searchCharacters}>Search</Button>
-      </Flex>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Box mb="4">
+          <FormControl isInvalid={!!errors.name}>
+            <Flex direction="row" justify="flex-start">
+              <Input id="name" {...register("name", {required: "Birdperson needs you to type a name", minLength: 1})} placeholder="Search a character" variant="solid" mb="5" maxW="400px" />       
+              <Button type="submit" colorScheme="green" ml="5" isLoading={isSubmitting}>Search</Button>
+            </Flex>
+            <FormErrorMessage>{!!errors.name && errors.name.message?.toString()}</FormErrorMessage>
+          </FormControl>
+        </Box>
+      </form>
       <CharacterTable data={data ?? []} />
       <Flex justify="center" m="5">
         <Flex direction="row" gap="5">
@@ -65,3 +80,4 @@ export default function Home() {
     </Box>
   );
 }
+
