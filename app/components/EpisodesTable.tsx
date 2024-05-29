fc8c670ab3c9924c37d348748d638af63104a267
Jan <jandetlefsen@gmail.com>
2024-05-29 21:19:03 +0200

    chore(rick-and-morty): :recycle: fixed formatting

--- app/components/EpisodesTable.tsx
+++ app/components/EpisodesTable.tsx
@@ -3,76 +3,79 @@ import DataTable, { TableColumn } from "react-data-table-component";
 import { Episode, getEpisode } from "rickmortyapi";
 
 interface EpisodeTableProps {
-    episodeUrlArray: string[];
+	episodeUrlArray: string[];
 }
 
 const episodeColumns: TableColumn<Episode>[] = [
-    {
-        name: 'Episode Name',
-        width: '300px',
-        selector: row => row.name,
-    },
-    // get the episode number from the the row.episode string
-    {
-        name: 'Season',
-        selector: row => {
-            const seasonCode = row.episode.split('E')[0].split('S')[1]
-            return seasonCode
-        },
-    },
-    {
-        name: 'Episode',
-        selector: row => {
-            const episodeCode = row.episode.split('E')[1].split('S')[0]
-            return episodeCode
-        
-        },
-    },
+	{
+		name: "Episode Name",
+		width: "300px",
+		selector: (row) => row.name,
+	},
+	// get the episode number from the the row.episode string
+	{
+		name: "Season",
+		selector: (row) => {
+			const seasonCode = row.episode.split("E")[0].split("S")[1];
+			return seasonCode;
+		},
+	},
+	{
+		name: "Episode",
+		selector: (row) => {
+			const episodeCode = row.episode.split("E")[1].split("S")[0];
+			return episodeCode;
+		},
+	},
 ];
 
-
 const getEpisodeIds = (episodeUrl: string[]) => {
+	let episodeCodes: number[] = [0]; //HACK: for some reason this is needed to prevent characters with only one episode from not rendering episodes
 
-    let episodeCodes: number[] = [0]; //HACK: for some reason this is needed to prevent characters with only one episode from not rendering episodes
+	if (episodeUrl === undefined) console.log("episodeUrl is undefined");
 
-    if(episodeUrl === undefined)
-    console.log("episodeUrl is undefined")
-    
-    episodeUrl?.forEach(url => {
-      //get the last number in the url
-        const urlArray = url.split('/')
-        const episodeNumber = urlArray[urlArray.length - 1]
-        episodeCodes.push(parseInt(episodeNumber))
-    })
-    
-    return episodeCodes
-}
-    
+	episodeUrl?.forEach((url) => {
+		//get the last number in the url
+		const urlArray = url.split("/");
+		const episodeNumber = urlArray[urlArray.length - 1];
+		episodeCodes.push(parseInt(episodeNumber));
+	});
+
+	return episodeCodes;
+};
+
 export default function EpisodeTable({ episodeUrlArray }: EpisodeTableProps) {
+	const paginationComponentOptions = {
+		noRowsPerPage: true,
+	};
 
-    const paginationComponentOptions = {
-	    noRowsPerPage: true,
-    };
+	//TODO: Make this a useReducer function
+	const [episodeIds, setEpisodeIds] = useState<number[]>([]);
 
-    const [episodeIds, setEpisodeIds] = useState<number[]>([])
+	const [episodeData, setEpisodeData] = useState<Episode[]>([]); //All characters are in an episode
 
-    const [episodeData, setEpisodeData] = useState<Episode[]>([]) //All characters are in an episode
+	// get episode ids from the url array
+	useEffect(() => {
+		setEpisodeIds(getEpisodeIds(episodeUrlArray));
+	}, [episodeUrlArray]);
 
+	useEffect(() => {
+		getEpisode(episodeIds)
+			.then((result) => {
+				setEpisodeData(result.data.map((item: Episode) => item));
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	}, [episodeIds]);
 
-    // get episode ids from the url array
-    useEffect(() => {
-        setEpisodeIds(getEpisodeIds(episodeUrlArray))
-    }, [episodeUrlArray])
-
-    useEffect(() => {
-        getEpisode(episodeIds).then(result => {
-            setEpisodeData(result.data.map((item: Episode) => item))
-        }).catch(err => {   
-            console.log(err)
-        })   
-    }, [episodeIds])
-
-    return (
-        <DataTable columns={episodeColumns} data={episodeData ?? []} dense pagination={episodeData?.length > 10 ? true : false}  paginationComponentOptions={paginationComponentOptions} />
-    )
+	return (
+		<DataTable
+			columns={episodeColumns}
+			data={episodeData ?? []}
+			dense
+			pagination={episodeData?.length > 10 ? true : false}
+			paginationComponentOptions={paginationComponentOptions}
+		/>
+	);
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,73 +1,116 @@
 "use client";
 
-import { Box, Button, Flex, FormControl, Input, Text, useTheme } from "@chakra-ui/react";
+import {
+	Box,
+	Button,
+	Flex,
+	FormControl,
+	Input,
+	Text,
+	useTheme,
+} from "@chakra-ui/react";
 import { useEffect, useMemo, useState } from "react";
-import { useForm } from 'react-hook-form';
+import { FieldValues, useForm } from "react-hook-form";
 import { ApiResponse, Character, Info, getCharacters } from "rickmortyapi"; //Use this to get types and easier access to the API
 import CharacterTable from "./components/CharacterTable";
 
 export default function Home() {
+	const theme = useTheme();
 
-  const theme = useTheme()
+	const [data, setData] = useState<ApiResponse<Info<Character[]>>>();
 
-  const [data, setData] = useState<ApiResponse<Info<Character[]>>>()
+	// TODO: Maybe use a useReducer for search and page states
+	const [search, setSearch] = useState<string>("");
 
-  const [search, setSearch] = useState<string>("")
+	const [currentPage, setCurrentPage] = useState<number>(1);
 
-  const [currentPage, setCurrentPage] = useState<number>(1)
+	const [totaltPages, setTotalPages] = useState<number>(1);
 
-  const [totaltPages, setTotalPages] = useState<number>(1)
+	useEffect(() => {
+		getCharacters({ name: search, page: currentPage })
+			.then((result) => {
+				setData(result);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	}, [currentPage, search]);
 
-  useEffect(() => {
-    getCharacters({ name: search, page: currentPage }).then(result => {
-      setData(result)
-    }).catch(err => {
-      console.log(err)
-    })
-  },[currentPage, search])
+	const characters = useMemo(() => {
+		setTotalPages(data?.data.info?.pages ?? 1);
+		return data?.data.results?.map((item: Character) => item) ?? [];
+	}, [data]);
 
-  const characters = useMemo(() => {
-    setTotalPages(data?.data.info?.pages ?? 1)
-    return data?.data.results?.map((item: Character) => item) ?? []
-  },[data])
+	const {
+		handleSubmit,
+		register,
+		formState: { errors, isSubmitting },
+	} = useForm();
 
-  const {
-    handleSubmit,
-    register,
-    formState: { errors, isSubmitting },
-  } = useForm()
+	function onSubmit(values: FieldValues) {
+		const name = values.name;
+		setSearch(name); // Remember for swithing pages
+		setCurrentPage(1); // make sure we view the first page of the name search. Else, we might end on a page that doesn't exist
+	}
 
-  function onSubmit(values: any) {
-    const name = values.name //You shouldn't use any type, but I'm lazy right now
-    setSearch(name) // Remember for swithing pages
-    setCurrentPage(1) // make sure we view the first page of the name search. Else, we might end on a page that doesn't exist
-  }
+	function gotoPage(newPage: number) {
+		setCurrentPage(newPage);
+	}
 
-  function gotoPage(newPage: number) {
-    setCurrentPage(newPage)
-  }
-
-  return (
-    <Box p="5" w="100vw" h="100vh" bg={theme.colors.gray[200]} overflow="scroll">
-      <Text fontSize="3xl" mb="5">Rick and Morty Characters</Text>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <Box mb="4">
-          <FormControl>
-            <Flex direction="row" justify="flex-start">
-              <Input id="name" {...register("name")} placeholder="Search a character" variant="solid" mb="5" maxW="400px" />       
-              <Button type="submit" colorScheme="green" ml="5" isLoading={isSubmitting}>Search</Button>
-            </Flex>
-          </FormControl>
-        </Box>
-      </form>
-      <CharacterTable data={characters ?? []} />
-      <Flex justify="center" m="5">
-        <Flex direction="row" gap="5">
-          <Button colorScheme="green" onClick={()=> gotoPage(currentPage - 1)} isDisabled={currentPage === 1}>Previous</Button>
-          <Button colorScheme="green" onClick={()=> gotoPage(currentPage + 1)} isDisabled={currentPage === totaltPages}>Next</Button>
-        </Flex>
-      </Flex>
-    </Box>
-  );
+	return (
+		<Box
+			p="5"
+			w="100vw"
+			h="100vh"
+			bg={theme.colors.gray[200]}
+			overflow="scroll"
+		>
+			<Text fontSize="3xl" mb="5">
+				Rick and Morty Characters
+			</Text>
+			<form onSubmit={handleSubmit(onSubmit)}>
+				<Box mb="4">
+					<FormControl>
+						<Flex direction="row" justify="flex-start">
+							<Input
+								id="name"
+								{...register("name")}
+								placeholder="Search a character"
+								variant="solid"
+								mb="5"
+								maxW="400px"
+							/>
+							<Button
+								type="submit"
+								colorScheme="green"
+								ml="5"
+								isLoading={isSubmitting}
+							>
+								Search
+							</Button>
+						</Flex>
+					</FormControl>
+				</Box>
+			</form>
+			<CharacterTable data={characters ?? []} />
+			<Flex justify="center" m="5">
+				<Flex direction="row" gap="5">
+					<Button
+						colorScheme="green"
+						onClick={() => gotoPage(currentPage - 1)}
+						isDisabled={currentPage === 1}
+					>
+						Previous
+					</Button>
+					<Button
+						colorScheme="green"
+						onClick={() => gotoPage(currentPage + 1)}
+						isDisabled={currentPage === totaltPages}
+					>
+						Next
+					</Button>
+				</Flex>
+			</Flex>
+		</Box>
+	);
 }
-
